a9e4612ea9d96bdc80e5104c6b640568eeecdffd
V-ishnuvarthan <vishnuvarthan88707@gmail.com>
2024-08-05 19:01:50 +0530

    forgot password functionality

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function hideChildMenu(X) {
 var modal = document.querySelector('.modal');
 var btn = document.querySelector('.signupBtn');
 var span = document.querySelector('.modal__close');
-
 btn.onclick = function() {
     modal.style.display = 'block';
 }
@@ -29,7 +28,6 @@ var signupModal = document.querySelector('.modal--signup');
 var regBtn = document.querySelector('.modal__register-link');
 var regClose = document.querySelector('.form__close');
 var loginRedirect = document.querySelector('.form__login-link');
-
 regBtn.onclick = function() {
     modal.style.display = 'none';
     signupModal.style.display = 'flex';
@@ -42,7 +40,28 @@ loginRedirect.onclick = function() {
     modal.style.display = 'block';
 }
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == signupModal) {
         signupModal.style.display = 'none';
+    }
+}
+
+var forgotPassModal = document.querySelector('.modal__forgotPassword');
+var forgotBtn = document.querySelector('.modal__input-forgotPassword');
+var forgotModalClose = document.querySelector('.forgot__modal--close');
+var loginPage = document.querySelector('.forgot__login-link');
+forgotBtn.onclick = function() {
+    modal.style.display = 'none';
+    forgotPassModal.style.display = 'block';
+}
+forgotModalClose.onclick = function() {
+    forgotPassModal.style.display = 'none';
+}
+loginPage.onclick = function() {
+    modal.style.display = 'block';
+    forgotPassModal.style.display = 'none';
+}
+window.onclick = function(event) {
+    if (event.target == forgotPassModal) {
+        forgotPassModal.style.display = 'none';
     }
 }
