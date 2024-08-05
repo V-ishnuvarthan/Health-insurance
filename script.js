f46f14444408ed337fcc2ce272a000c32da724e3
V-ishnuvarthan <vishnuvarthan88707@gmail.com>
2024-08-05 17:54:27 +0530

    signup functionality

--- script.js
+++ script.js
@@ -8,17 +8,41 @@ function hideChildMenu(X) {
     ele.classList.remove("showChildMenu");
 }
 
-console.log(document.querySelector('.signupBtn'));
-document.querySelector('.signupBtn').onclick = function() {
-    document.querySelector('.signupModal').style.display = "block";
-}
+// Get the modal
+var modal = document.querySelector('.modal');
+var btn = document.querySelector('.signupBtn');
+var span = document.querySelector('.modal__close');
 
-document.querySelector('.close').onclick = function() {
-    document.querySelector('.signupModal').style.display = "none";
+btn.onclick = function() {
+    modal.style.display = 'block';
 }
-
+span.onclick = function() {
+    modal.style.display = 'none';
+}
 window.onclick = function(event) {
-    if (event.target == document.querySelector('.signupModal')) {
-        document.querySelector('.signupModal').style.display = "none";
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+
+var signupModal = document.querySelector('.modal--signup');
+var regBtn = document.querySelector('.modal__register-link');
+var regClose = document.querySelector('.form__close');
+var loginRedirect = document.querySelector('.form__login-link');
+
+regBtn.onclick = function() {
+    modal.style.display = 'none';
+    signupModal.style.display = 'flex';
+}
+regClose.onclick = function() {
+    signupModal.style.display = 'none';
+}
+loginRedirect.onclick = function() {
+    signupModal.style.display = 'none';
+    modal.style.display = 'block';
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        signupModal.style.display = 'none';
     }
 }
